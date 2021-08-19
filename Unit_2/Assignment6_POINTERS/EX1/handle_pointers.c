445a6ca4158d1ee7e920765214bee3145df9fb70
Salma Sherif <salmasherif294@gmail.com>
2021-08-19 02:56:29 +0200

    added example for handling pointers

---
+++ Unit_2/Assignment6_POINTERS/EX1/handle_pointers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+
+	int m = 29;
+	printf("Address of m: %p\n", &m);
+	printf("Value of m : %d\n",m);
+
+	printf("Now ab is assigned with the address of m\n");
+	int *ab = &m;
+	printf("Address of pointer ab : %p\n", ab);
+	printf("Content of pointer ab : %d\n",*ab);
+
+	printf("The value of m assigned to 34 now.\n");
+	m = 34;
+	printf("Address of pointer ab : %p\n",ab);
+	printf("Content of pointer ab : %d\n",*ab);
+
+	printf("The pointer variable ab is assigned with the value 7 now\n");
+	*ab = 7;
+	printf("Address of m: %p\n", &m);
+	printf("Value of m : %d\n",m);
+
+
+	return 0;
+}
