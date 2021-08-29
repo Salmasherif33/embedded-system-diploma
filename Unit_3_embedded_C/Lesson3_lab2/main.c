75378e48f5951006c3ae2997257a84ff9b1588d6
Salma <salmasherif294@gmail.com>
2021-08-29 03:38:46 +0200

    toogle_led_from_scratch_startup_&_Linker

---
+++ Unit_3_embedded_C/Lesson3_lab2/main.c
@@ -0,0 +1,53 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Salma Sherif
+ * @brief          : Toggle Led on STMF103C6
+ ******************************************************************************
+ 
+ */
+
+
+typedef volatile unsigned int vuint32_t;
+
+#include "stdint.h"
+
+//register adresses
+#define RCC_BASE   0x40021000
+#define PortA_BASE 0x40010800
+
+#define RCC_APB2ENR  *(volatile uint32_t *) (RCC_BASE + 0x18)
+#define GPIOA_CRH    *(volatile uint32_t *) (PortA_BASE + 0x04)
+#define GPIOA_ODR    *(volatile uint32_t *) (PortA_BASE + 0x0C)
+
+//bit fileds
+
+
+typedef union{
+	vuint32_t all_fields; //32bits
+	struct{
+		vuint32_t reserved: 13;
+		vuint32_t P_13: 1;
+	}Pin;
+}R_ODR_t;
+
+volatile R_ODR_t* R_ODR = (volatile R_ODR_t*)(PortA_BASE + 0x0C);
+unsigned char var[3] = {1,2,3};
+unsigned char const c_var[3] = {1,2,3};
+
+int main(void)
+{
+	RCC_APB2ENR |= 1<<2; //set bit 13
+	GPIOA_CRH &= 0xFF0FFFFF;
+	GPIOA_CRH |= 0x00200000;
+	while(1){
+
+		R_ODR->Pin.P_13 = 1;
+		for(int j = 0; j < 5000; j++); //delay
+		R_ODR->Pin.P_13 = 0;
+		//GPIOA_ODR ^= (1<<13);
+		for(int j = 0; j< 5000; j++);
+
+	}
+
+}
---
+++ Unit_3_embedded_C/Lesson3_lab2/startup.c
@@ -0,0 +1,74 @@
+#include <stdint.h>
+//#define Stack_Top 0x20001000
+
+//extern from linker file
+extern unsigned int _stack_top;
+
+extern unsigned int _E_text;
+extern unsigned int _E_DATA;
+extern unsigned int _S_DATA;
+extern unsigned int _E_bss;
+extern unsigned int _S_bss;
+
+//extern from main.c
+extern int main(void);
+
+void Reset_Handler(void);
+
+void Default_Handler(void){
+	
+	Reset_Handler();
+	
+}
+
+void NMI_Handler(void) {
+	
+}
+	
+void H_fault_Handler(void) {
+	
+}
+
+void MM_Fault_Handler(void) {}
+	
+void Bus_Fault(void) {}
+	
+void Usage_Fault_Handler(void) {}
+	
+
+
+uint32_t vectors [] __attribute__((section(".vectors"))) = {
+	(uint32_t) &_stack_top,
+	(uint32_t) &Reset_Handler,
+	(uint32_t) &NMI_Handler,
+	(uint32_t) &H_fault_Handler,
+	(uint32_t) &MM_Fault_Handler,
+	(uint32_t) &Bus_Fault,
+	(uint32_t) &Usage_Fault_Handler
+};
+
+
+void Reset_Handler(void){
+	//copy data section from rom to sram
+	unsigned int data_size = (unsigned char*)&_E_DATA - (unsigned char*)&_S_DATA ;  //copy byte by byte even if there is an alignment, casting as it is a symbol
+	unsigned char* src = (unsigned char*)&_E_text;
+	unsigned char* dest = (unsigned char*)&_S_DATA;
+	
+	int i;
+	for(i = 0; i < data_size; i++){
+		*((unsigned char*)dest++) = *((unsigned char*)src++);
+	}
+	
+	//reserve a space for bss in sram and init it with 0
+	unsigned int bss_size = (unsigned char*)&_E_bss - (unsigned char*)&_S_bss;
+	dest = (unsigned char*)&_S_bss;
+	
+	for(i = 0; i < bss_size; i++){
+		*((unsigned char*)dest++) = (unsigned char)0;
+	}
+	
+	
+	main();
+	
+}
+
